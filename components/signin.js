cf6199aaf8d0a3519fe6fd2e6d36d1ceb70e4137
Khem Sok <khemsok97@gmail.com>
2020-08-15 23:11:45 -0400

    Persist state with authentication

--- components/signin.js
+++ components/signin.js
@@ -16,30 +16,36 @@ function SignIn() {
 
     firebase
       .auth()
-      .signInWithPopup(provider)
-      .then(function (result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-
-        firebase
+      .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .then(() => {
+        return firebase
           .auth()
-          .currentUser.getIdToken(true)
-          .then((idToken) => {
-            fetch(new URL("/api/auth", document.baseURI), {
-              headers: {
-                Authorization: `${idToken}`,
-              },
-            })
-              .then((res) => res.json())
-              .then((data) => {
-                Router.reload("/");
-              })
-              .catch((err) => console.error(err));
+          .signInWithPopup(provider)
+          .then(function (result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+
+            firebase
+              .auth()
+              .currentUser.getIdToken(true)
+              .then((idToken) => {
+                fetch(new URL("/api/auth", document.baseURI), {
+                  headers: {
+                    Authorization: `${idToken}`,
+                  },
+                })
+                  .then((res) => res.json())
+                  .then((data) => {
+                    Router.reload("/");
+                  })
+                  .catch((err) => console.error(err));
+              });
+            // ...
           });
-        // ...
       })
+
       .catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
